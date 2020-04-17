9b2449654f7d1f70c363f512c138dabe025a055c
vkir_itacademy <valyushko.kirill@gmail.com>
2020-04-17 18:49:19 +0300

    fix

--- src/lecture/week5/collections/set/example/SimpleHashSet.java
+++ src/lecture/week5/collections/set/example/SimpleHashSet.java
@@ -1,40 +1,23 @@
 package lecture.week5.collections.set.example;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.HashSet;
 import java.util.Set;
 
 public class SimpleHashSet {
 
     public static void main(String args[]) {
+        Set<String> set = new HashSet<String>();
 
-        // Create a hash map.
-        Map<String, Double> hm = new HashMap<String, Double>();
+        // Add elements to the hash set.
+        set.add("Beta");
+        set.add("Alpha");
+        set.add("Eta");
+        set.add("Gamma");
+        set.add("Epsilon");
+        set.add("Omega");
+        set.add("Omega");
 
-        // Put elements to the map
-        hm.put("John Doe", new Double(3434.34));
-        hm.put("Tom Smith", new Double(123.22));
-        hm.put("Jane Baker", new Double(1378.00));
-        hm.put("Tod Hall", new Double(99.22));
-        hm.put("Ralph Smith", new Double(-19.08));
-
-        // Get a set of the entries.
-        Set<Map.Entry<String, Double>> set = hm.entrySet();
-
-        // Display the set.
-        for (Map.Entry<String, Double> me : set) {
-            System.out.print(me.getKey() + ": ");
-            System.out.println(me.getValue());
-        }
-
-        System.out.println();
-
-        // Deposit 1000 into John Doe's account.
-        double balance = hm.get("John Doe");
-        hm.put("John Doe", balance + 1000);
-
-        System.out.println("John Doe's new balance: " +
-                hm.get("John Doe"));
+        System.out.println(set);
     }
 
 }
